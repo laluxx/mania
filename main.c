b5d8d2e8ee4201e3326032a415645b8cc5220310
laluxx <lorenzoaddamo5@gmail.com>
2023-10-07 00:00:32 +0000

    Songs

--- main.c
+++ main.c
@@ -2,16 +2,20 @@
 #include "math.h"
 #include <stddef.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define COLUMNS 4
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 1000
 
 #define NOTE_RADIUS 55
 #define TARGET_RADIUS (NOTE_RADIUS + 5)  // TARGET_RADIUS is 5 units larger than NOTE_RADIUS
 #define TARGET_SPACING (2.8 * NOTE_RADIUS) // TARGET_SPACING is 3 times the NOTE_RADIUS
 
 
-#define MAX_NOTES 100
+#define MAX_NOTES 1000
 
 typedef struct {
     Vector2 position;
@@ -28,7 +32,9 @@ double hitFade = 0;
 bool autoPlay = false;
 Sound hitSound;
 
-
+int clickCount = 0;
+float cps = 0;
+double lastTimeUpdated = 0;
 
 
 void InitializeNotes() {
@@ -36,7 +42,7 @@ void InitializeNotes() {
         for (int j = 0; j < MAX_NOTES; j += (i % 2 == 0 ? 3 : 5)) { // More varied spacing
             notes[i][j] = (Note) { 
                 .position = { SCREEN_WIDTH / 2 + TARGET_SPACING * (i - COLUMNS / 2 + 0.5f), -NOTE_RADIUS * (j+1) * 6 }, 
-                .speed = 10,
+                .speed = 15,
                 .active = true, 
                 .hitTime = -1 
             };
@@ -44,16 +50,23 @@ void InitializeNotes() {
     }
 }
 
-
 bool GetKeyPress(int column) {
+    bool pressed = false;
     switch(column) {
-        case 0: return IsKeyPressed(KEY_D);
-        case 1: return IsKeyPressed(KEY_F);
-        case 2: return IsKeyPressed(KEY_J);
-        case 3: return IsKeyPressed(KEY_K);
-        default: return false;
+        case 0: pressed = IsKeyPressed(KEY_D); break;
+        case 1: pressed = IsKeyPressed(KEY_F); break;
+        case 2: pressed = IsKeyPressed(KEY_J); break;
+        case 3: pressed = IsKeyPressed(KEY_K); break;
     }
+    
+    if (pressed){
+    clickCount++;
+    }
+
+    return pressed;
 }
+
+
 
 Note* GetClosestNote(int column) {
     Note* closestNote = NULL;
@@ -88,6 +101,11 @@ void HandleNoteHit(Note* note, float distance) {
     
     // Play the sound every time the function is called, regardless of autoplay or manual hit
     PlaySound(hitSound);
+
+    if (autoPlay) {
+    clickCount++;
+    }
+
 }
 
 void MoveNotes() {
@@ -119,8 +137,32 @@ void UpdateNotes() {
     MoveNotes();
 
     if (hitFade > 0) hitFade -= GetFrameTime();
+
+
+  // Update CPS
+    if (GetTime() - lastTimeUpdated >= 1.0) {
+        cps = clickCount;
+        clickCount = 0;
+        lastTimeUpdated = GetTime();
+    }
+
 }
 
+// functions to move into separate files
+
+
+Color Lerp(Color a, Color b, float t) {
+    if (t < 0.0f) t = 0.0f;
+    if (t > 1.0f) t = 1.0f;
+
+    Color result;
+    result.r = a.r + (b.r - a.r) * t;
+    result.g = a.g + (b.g - a.g) * t;
+    result.b = a.b + (b.b - a.b) * t;
+    result.a = a.a + (b.a - a.a) * t;
+
+    return result;
+}
 
 void Render() {
     BeginDrawing();
@@ -153,38 +195,27 @@ void Render() {
         }
         DrawText(hitText, SCREEN_WIDTH/2 - MeasureText(hitText, 30)/2, SCREEN_HEIGHT/2 - 15, 30, Fade(hitColor, hitFade));
     }
-    
+
+    // Render CPS counter with dynamic effects
+    char cpsText[50];
+    sprintf(cpsText, "CPS: %.2f", cps);
+    Vector2 position = { SCREEN_WIDTH - MeasureText(cpsText, 24) - 10, 10 };  // Top-right corner with some padding
+
+    Color cpsColor = GREEN;
+    float cpsFontSize = 24;
+    if (cps > 5 && cps <= 10) {
+        cpsColor = Lerp(GREEN, YELLOW, (cps - 5) / 5);
+    } else if (cps > 10) {
+        cpsColor = Lerp(YELLOW, RED, (cps - 10) / 10);
+        position.x += (rand() % 8) - 4;  // Random movement for the "crazy" effect
+        position.y += (rand() % 8) - 4;
+        cpsFontSize += (cps - 10);  // Increase font size a bit for emphasis
+    }
+
+    DrawText(cpsText, position.x, position.y, cpsFontSize, cpsColor);
+
     EndDrawing();
 }
-
-// int main(void) {
-//     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Rhythm Game");
-//     SetTargetFPS(144);
-//     
-//     InitializeNotes(); // Initial setup
-//
-//     while (!WindowShouldClose()) {
-//
-//         if (IsKeyPressed(KEY_A)) {
-//             autoPlay = !autoPlay; // Toggle autoplay mode
-//         }
-//
-//
-//         if (IsKeyPressed(KEY_R)) {
-//             InitializeNotes();
-//         }
-//
-//
-//
-//         UpdateNotes();
-//         Render();
-//     }
-//
-//     CloseWindow();
-//     return 0;
-// }
-//
-
 
 int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Rhythm Game");
